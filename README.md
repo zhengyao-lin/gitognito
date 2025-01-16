6a77fe2ac01575715a9c58702b0f0d1e9490e574
Zhengyao Lin <zhengyao-lin@outlook.com>
2025-01-16 12:17:59 -0500

    Add more flags

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ Install with
 python3 -m pip install git+https://github.com/zhengyao-lin/gitognito.git
 ```
 
+Usage:
+```
+gitognito <source repo> <target repo> <target branch> [--force]
+```
--- gitognito/main.py
+++ gitognito/main.py
@@ -8,15 +8,18 @@ from git import Repo, Actor
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("repo", help="URL to the original Git repository")
+    parser.add_argument("source", help="URL to the original Git repository")
     parser.add_argument("target", help="URL to the target Git repository")
     parser.add_argument("target_branch", help="Branch to push in the target repository")
     parser.add_argument("-f", "--force", action="store_true", default=False, help="Force push to the target repository")
+    parser.add_argument("-a", "--author", default="Anonymous Author(s)", help="Author name to use in the final commit")
+    parser.add_argument("-e", "--email", default="anon@nowhere.com", help="Email to use in the final commit")
+    parser.add_argument("-m", "--message", default="Init", help="Message to use in the final commit")
     args = parser.parse_args()
 
     with TemporaryDirectory() as tmp_dir:
         print("cloning original repo...")
-        repo = Repo.clone_from(args.repo, os.path.join(tmp_dir, "original"))
+        repo = Repo.clone_from(args.source, os.path.join(tmp_dir, "original"))
 
         print("flattening submodules...")
         # Recursively initialize all submodules first
@@ -37,7 +40,7 @@ def main():
         print("anonymizing history...")
         repo = Repo.init(repo.working_tree_dir)
         repo.git.add(all=True)
-        anon = Actor("Anonymized Author(s)", "anon@anon.com")
+        anon = Actor(args.author, args.email)
         repo.index.commit("Init", author=anon, committer=anon)
 
         # Push to the target repo
